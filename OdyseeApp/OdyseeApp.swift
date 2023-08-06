238a3c1a7ca68ff4c1b93e11da6eb290e02fdf26
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-08-06 16:04:45 -0400

    Created SwiftUI app

---
+++ OdyseeApp/OdyseeApp.swift
@@ -0,0 +1,18 @@
+//
+//  OdyseeApp.swift
+//  OdyseeApp
+//
+//  Created by Alsey Coleman Miller on 8/5/23.
+//
+
+import SwiftUI
+
+@main
+struct OdyseeApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            EmptyView()
+        }
+    }
+}
---
+++ OdyseeApp/Views/TrendingContentView.swift
@@ -0,0 +1,18 @@
+//
+//  TrendingContentView.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 8/6/23.
+//
+
+import Foundation
+import SwiftUI
+import Odysee
+
+struct TrendingContentView: View {
+    
+    var body: some View {
+        
+        
+    }
+}
