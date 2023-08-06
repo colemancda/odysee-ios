9c33dde53b603b2dc3fc408c9a9228c48cb70c69
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-08-06 16:06:19 -0400

    Add `Store.loadPreferences()`

---
+++ OdyseeKit/Model/StorePreferences.swift
@@ -0,0 +1,29 @@
+//
+//  StorePreferences.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 8/6/23.
+//
+
+import Foundation
+
+public extension Store {
+    
+    internal(set) var username: String? {
+        get { preferences.username }
+        set { preferences.username = newValue }
+    }
+}
+
+internal extension Store {
+    
+    func loadPreferences() -> Preferences {
+        let preferences = Preferences.standard
+        preferencesObserver = preferences.objectWillChange
+            .receive(on: DispatchQueue.main)
+            .sink { [unowned self] in
+                self.objectWillChange.send()
+            }
+        return preferences
+    }
+}
