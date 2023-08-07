877cdac7ac6127556a7660f5f2e050383746d84b
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-08-06 20:00:51 -0400

    Add `Store.log()`

---
+++ OdyseeKit/Model/StoreLog.swift
@@ -0,0 +1,16 @@
+//
+//  StoreLog.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 8/6/23.
+//
+
+import Foundation
+
+public extension Store {
+    
+    func log(_ message: String) {
+        print(message)
+    }
+    
+}
